e7a4794e079cec84d1fe3aeb9649e53df093171e
Anh Nguyen <ngvanhfw@gmail.com>
2024-04-26 02:16:04 +0700

    lingo: modifying enumerable methods

--- enumerable.go
+++ enumerable.go
@@ -59,3 +59,73 @@ func AsEnumerableTFromSliceAny[T any](a []any) Enumerable[T] {
 		iterator: out,
 	}
 }
+
+// Concat concatenates two sequences.
+func (e Enumerable[T]) Concat(second Enumerable[T]) Enumerable[T] {
+	out := make(chan T)
+
+	go func() {
+		defer close(out)
+		first := e.iterator
+		secondI := second.iterator
+		for value := range first {
+			out <- value
+		}
+		for value := range secondI {
+			out <- value
+		}
+	}()
+
+	return Enumerable[T]{
+		iterator: out,
+	}
+}
+
+// Empty returns an empty Enumerable[T] that has the specified type argument.
+func Empty[T any]() Enumerable[T] {
+	out := make(chan T)
+
+	go func() {
+		defer close(out)
+	}()
+
+	return Enumerable[T]{
+		iterator: out,
+	}
+}
+
+// Range generates a sequence of integral numbers within a specified range.
+func Range(start, end int) Enumerable[int] {
+	out := make(chan int)
+
+	go func() {
+		defer close(out)
+		i := start
+		for i <= end {
+			out <- i
+			i++
+		}
+	}()
+
+	return Enumerable[int]{
+		iterator: out,
+	}
+}
+
+// Repeat generates a sequence that contains one repeated value.
+func Repeat[T any](element T, times int) Enumerable[T] {
+	out := make(chan T)
+
+	go func() {
+		defer close(out)
+		i := 0
+		for i < times {
+			out <- element
+			i++
+		}
+	}()
+
+	return Enumerable[T]{
+		iterator: out,
+	}
+}
---
+++ enumerable_test.go
@@ -0,0 +1,171 @@
+package lingo_test
+
+import (
+	"testing"
+
+	lingo "github.com/vanh01/lingo"
+)
+
+func TestConcat(t *testing.T) {
+	type args struct {
+		source []int
+		second []int
+		want   []int
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "Concat",
+			args: args{
+				source: []int{1, 2, 3},
+				second: []int{4, 5, 6},
+				want:   []int{1, 2, 3, 4, 5, 6},
+			},
+		},
+		{
+			name: "Concat",
+			args: args{
+				source: []int{},
+				second: []int{1, 2, 3},
+				want:   []int{1, 2, 3},
+			},
+		},
+		{
+			name: "Concat",
+			args: args{
+				source: []int{1, 2, 3},
+				second: []int{},
+				want:   []int{1, 2, 3},
+			},
+		},
+		{
+			name: "Concat",
+			args: args{
+				source: []int{},
+				second: []int{},
+				want:   []int{},
+			},
+		},
+		{
+			name: "Concat",
+			args: args{
+				want: []int{},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := lingo.AsEnumerable(tt.args.source).Concat(lingo.AsEnumerable(tt.args.second)).ToSlice()
+			if len(got) != len(tt.args.want) {
+				t.Errorf("%s() = %v, want %v", tt.name, got, tt.args.want)
+			}
+			for i := range tt.args.want {
+				if got[i] != tt.args.want[i] {
+					t.Errorf("%s() = %v, want %v", tt.name, got[i], tt.args.want[i])
+				}
+			}
+		})
+	}
+}
+
+func TestEmpty(t *testing.T) {
+	type args struct {
+		second []int
+		want   []int
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "Empty",
+			args: args{
+				second: []int{1, 2, 3},
+				want:   []int{1, 2, 3},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := lingo.Empty[int]().Concat(lingo.AsEnumerable(tt.args.second)).ToSlice()
+			if len(got) != len(tt.args.want) {
+				t.Errorf("%s() = %v", tt.name, got)
+			}
+			for i := range tt.args.want {
+				if got[i] != tt.args.want[i] {
+					t.Errorf("%s() = %v, want %v", tt.name, got[i], tt.args.want[i])
+				}
+			}
+		})
+	}
+}
+
+func TestRange(t *testing.T) {
+	type args struct {
+		start int
+		end   int
+		want  []int
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "Range",
+			args: args{
+				start: 1,
+				end:   3,
+				want:  []int{1, 2, 3},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := lingo.Range(tt.args.start, tt.args.end).ToSlice()
+			if len(got) != len(tt.args.want) {
+				t.Errorf("%s() = %v", tt.name, got)
+			}
+			for i := range tt.args.want {
+				if got[i] != tt.args.want[i] {
+					t.Errorf("%s() = %v, want %v", tt.name, got[i], tt.args.want[i])
+				}
+			}
+		})
+	}
+}
+
+func TestRepeat(t *testing.T) {
+	type args struct {
+		value int
+		times int
+		want  []int
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "Range",
+			args: args{
+				value: 1,
+				times: 3,
+				want:  []int{1, 1, 1},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := lingo.Repeat(tt.args.value, tt.args.times).ToSlice()
+			if len(got) != len(tt.args.want) {
+				t.Errorf("%s() = %v", tt.name, got)
+			}
+			for i := range tt.args.want {
+				if got[i] != tt.args.want[i] {
+					t.Errorf("%s() = %v, want %v", tt.name, got[i], tt.args.want[i])
+				}
+			}
+		})
+	}
+}
