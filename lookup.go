a8e58bf6b5cae733f5616250c408017f155e7a71
Anh Nguyen <ngvanhfw@gmail.com>
2024-05-13 00:35:23 +0700

    lingo: add as plookup method

--- lookup.go
+++ lookup.go
@@ -1,6 +1,7 @@
 package lingo
 
 import (
+	"sync"
 	"unsafe"
 
 	"github.com/vanh01/lingo/definition"
@@ -58,6 +59,105 @@ func AsLookup[T any, K any, V any](
 			}
 		}
 		res[key] = append(res[key], element)
+	}
+
+	return Lookup[K, V]{
+		Count: len(res),
+		item:  res,
+		Enumerable: Enumerable[Grouping[K, V]]{
+			getIter: func() <-chan Grouping[K, V] {
+				out := make(chan Grouping[K, V])
+
+				go func() {
+					defer close(out)
+					// Initialize LookUp
+					for k := range res {
+						// must declare here to load data into memmory
+						temp := res[k]
+						out <- Grouping[K, V]{
+							Key:   k.(K),
+							Count: len(temp),
+							Enumerable: Enumerable[V]{
+								getIter: func() <-chan V {
+									outGrouping := make(chan V)
+
+									go func() {
+										defer close(outGrouping)
+										for i := range temp {
+											outGrouping <- temp[i]
+										}
+									}()
+
+									return outGrouping
+								},
+							},
+						}
+					}
+				}()
+
+				return out
+			},
+		},
+	}
+}
+
+// ParallelEnumerable
+
+// AsPLookup creates a generic Lookup[K, V] from an ParallelEnumerable[T].
+//
+// elementSelector can be nil. If elementSelector is not empty or nil, we will use the first elementSelector
+func AsPLookup[T any, K any, V any](
+	p ParallelEnumerable[T],
+	keySelector definition.SingleSelectorFull[T, K],
+	elementSelector ...definition.SingleSelectorFull[T, V],
+) Lookup[K, V] {
+	// group by section
+	type data struct {
+		key any
+		val V
+	}
+	mapdata := make(chan data)
+
+	go func() {
+		defer close(mapdata)
+		var wg sync.WaitGroup
+		for value := range p.getIter() {
+			wg.Add(1)
+			tempValue := value
+			go func() {
+				defer wg.Done()
+
+				ele := make(chan V)
+				go func() {
+					defer close(ele)
+					var element V
+					if !definition.IsEmptyOrNil(elementSelector) {
+						element = elementSelector[0](tempValue)
+					} else {
+						temp, ok := any(tempValue).(V)
+						if !ok {
+							element = *(*V)(unsafe.Pointer(&tempValue))
+						} else {
+							element = temp
+						}
+					}
+					ele <- element
+				}()
+
+				key := keySelector(tempValue)
+
+				mapdata <- data{
+					key: key,
+					val: <-ele,
+				}
+			}()
+		}
+		wg.Wait()
+	}()
+
+	res := map[any][]V{}
+	for d := range mapdata {
+		res[d.key] = append(res[d.key], d.val)
 	}
 
 	return Lookup[K, V]{
--- lookup_test.go
+++ lookup_test.go
@@ -271,3 +271,103 @@ func TestGetValue(t *testing.T) {
 		})
 	}
 }
+
+func TestAsPLookup(t *testing.T) {
+	type Student struct {
+		Id      int
+		Name    string
+		ClassId int
+	}
+	type args struct {
+		keySelector     definition.SingleSelectorFull[Student, int]
+		elementSelector definition.SingleSelectorFull[Student, any]
+	}
+	type want struct {
+		count int
+		item  map[int][]any
+	}
+	tests := []struct {
+		name   string
+		source []Student
+		args   args
+		want   want
+	}{
+		{
+			name: "AsLookup",
+			source: []Student{
+				{Id: 1, Name: "Anh", ClassId: 1},
+				{Id: 2, Name: "hnA", ClassId: 2},
+				{Id: 3, Name: "Abcd", ClassId: 3},
+				{Id: 4, Name: "Ank", ClassId: 1},
+				{Id: 5, Name: "hnI", ClassId: 2},
+				{Id: 6, Name: "A", ClassId: 3},
+			},
+			args: args{
+				keySelector: func(s Student) int {
+					return s.ClassId
+				},
+			},
+			want: want{
+				count: 3,
+				item: map[int][]any{
+					1: {
+						Student{Id: 1, Name: "Anh", ClassId: 1},
+						Student{Id: 4, Name: "Ank", ClassId: 1},
+					},
+					2: {
+						Student{Id: 2, Name: "hnA", ClassId: 2},
+						Student{Id: 5, Name: "hnI", ClassId: 2},
+					},
+					3: {
+						Student{Id: 3, Name: "Abcd", ClassId: 3},
+						Student{Id: 6, Name: "A", ClassId: 3},
+					},
+				},
+			},
+		},
+		{
+			name: "AsLookup",
+			source: []Student{
+				{Id: 1, Name: "Anh", ClassId: 1},
+				{Id: 2, Name: "hnA", ClassId: 2},
+				{Id: 3, Name: "Abcd", ClassId: 3},
+				{Id: 4, Name: "Ank", ClassId: 1},
+				{Id: 5, Name: "hnI", ClassId: 2},
+				{Id: 6, Name: "A", ClassId: 3},
+			},
+			args: args{
+				keySelector: func(s Student) int {
+					return s.ClassId
+				},
+				elementSelector: func(s Student) any {
+					return s.Name
+				},
+			},
+			want: want{
+				count: 3,
+				item: map[int][]any{
+					1: {"Anh", "Ank"},
+					2: {"hnA", "hnI"},
+					3: {"A", "Abcd"},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := lingo.AsPLookup(lingo.AsParallelEnumerable(tt.source), tt.args.keySelector, tt.args.elementSelector)
+			if got.Count != tt.want.count {
+				t.Errorf("%s() = %v, want %v", tt.name, got, tt.want)
+			}
+			for group := range got.GetIter() {
+				i := 0
+				for v := range group.OrderBy(func(a any) any { return a }).GetIter() {
+					if v != tt.want.item[group.Key][i] {
+						t.Errorf("%s() = %v, want %v", tt.name, v, tt.want.item[group.Key][i])
+					}
+					i++
+				}
+			}
+		})
+	}
+}
